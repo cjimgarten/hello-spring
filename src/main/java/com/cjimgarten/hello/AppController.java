84536b1abd49c30ab82a6518a9befee42a8dae4b
Chris Imgarten <cjimgarten@gmail.com>
2018-03-10 07:24:21 -0600

    add initial project files

---
+++ src/main/java/com/cjimgarten/hello/AppController.java
@@ -0,0 +1,48 @@
+package com.cjimgarten.hello;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by chris on 3/9/18.
+ */
+@Controller
+public class AppController {
+
+    private static final Logger LOGGER = Logger.getLogger(AppController.class);
+
+    @Value(value = "${com.cjimgarten.app.title}")
+    String title;
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String index(Model model) {
+
+        LOGGER.info("Get index page");
+
+        model.addAttribute("title", title);
+        return "index";
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public String processForm(@RequestParam String name, Model model) {
+
+        LOGGER.info("Processing form request");
+
+        String greeting = "Hello " + name;
+
+        if (name.equals("")) {
+            greeting = "Greetings";
+        }
+
+        LOGGER.info(greeting);
+
+        model.addAttribute("greeting", greeting);
+        model.addAttribute("title", title);
+        return "hello";
+    }
+}
